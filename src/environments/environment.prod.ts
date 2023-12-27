9062d1134386bd1a0d298376ca9c121bed3660c2
Padraic Reilly <patrick.reilly22@gmail.com>
2023-12-26 16:24:06 -0800

    built for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  AUTH_ENDPOINT:
+    "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token",
+  TOKEN_KEY: "whos-who-access-token"
 };
